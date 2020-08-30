866b231e6afb9e3b962d9a7a1f95c34530b40170
Alejandro Santiago <44524995+alestiago@users.noreply.github.com>
2020-08-30 21:38:53 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('#github').click(function(){
+    window.open('https://github.com/alestiago', '_blank').focus();
+  })
+  $('#linkedin').click(function(){
+    window.open('https://www.linkedin.com/in/alejandro-santiago-44259b159/', '_blank').focus();
+  })
+});
