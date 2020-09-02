c32972d11693a2936eee91ed4ece3cc339495377
Alejandro Santiago <44524995+alestiago@users.noreply.github.com>
2020-09-02 10:25:49 +0100

    Included twitter

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,11 @@
 $(function(){
-  $('#github').click(function(){
-    window.open('https://github.com/alestiago', '_blank').focus();
-  })
-  $('#linkedin').click(function(){
-    window.open('https://www.linkedin.com/in/alejandro-santiago-44259b159/', '_blank').focus();
-  })
+  function openURL($object, url) {
+    $object.click(function(){
+      window.open(url, '_blank').focus();
+    })
+  }
+
+  openURL($('#twitter'), 'https://twitter.com/alestiago3');
+  openURL($('#github'), 'https://github.com/alestiago');
+  openURL($('#linkedin'), 'https://www.linkedin.com/in/alejandro-santiago-44259b159/');
 });
