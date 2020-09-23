fbd0ed0a1efe30b13b651867b135ea695af3d0ef
Alejandro Santiago <44524995+alestiago@users.noreply.github.com>
2020-09-23 22:10:06 +0100

    Added email icon

--- docs/script.js
+++ docs/script.js
@@ -5,6 +5,7 @@ $(function(){
     })
   }
 
+  openURL($('#email'), 'mailto:me@alestiago.com');
   openURL($('#twitter'), 'https://twitter.com/alestiago3');
   openURL($('#github'), 'https://github.com/alestiago');
   openURL($('#linkedin'), 'https://www.linkedin.com/in/alejandro-santiago-44259b159/');
